d5fce4cf680ec682e015ee0011768408b481a0b0
Muhammad Hana Haikal <akuhaykal9gmail.com>
2023-10-25 18:44:11 +0700

    first commit

---
+++ application/views/global/home.php
@@ -0,0 +1,42 @@
+<div class="row">
+	<div class="col m 12">
+		<center>
+			<h5>Selamat Datang Di Sistem Informasi Perpustakaan SMA NEGERI 1 BALAPULANG</h5>
+		</center><hr>
+	</div>
+
+	<div class="row">
+		<div class="col m12">
+			<div class="card-panel teal darken-2 center white-text">
+				<p>
+                     "Buku adalah jendela dunia di mana kita bisa melihat isi dunia tanpa melakukan perjalanan, hanya cukup membaca sebuah halaman."
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/bb.jpg"> <!-- random image -->
+				</div>
+				<div class="card-content teal center">
+                    <a class="btn" href="<?php echo base_url();?>Buku/list_buku">Daftar Buku</a>
+				</div>
+			</div>
+		</div>
+
+        <div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/perpus.jpg"> <!-- random image -->
+				</div>
+				<div class="card-content teal center">
+                    <a class="btn" href="<?php echo base_url();?>Buku/profile">Tentang Perpustakaan</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+</div>
---
+++ application/views/global/profile.php
@@ -0,0 +1,33 @@
+<div class="row">
+	<div class="col m 12">
+		<h5>Profil Perpustakaan SMAN 1 BALAPULANG</h5>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col m6">
+		<div class="card-panel teal darken-2 white-text">
+			<h6><b>Visi</b></h6>
+			<p>
+				"Menjadi wadah pengetahuan digital yang inovatif dan terintegrasi untuk mendukung pembelajaran dan literasi di SMAN 1 Balapulang."
+			</p><br>
+			<h6><b>Misi</b></h6>
+			
+				1.	Terus mengikuti perkembangan teknologi untuk memperbarui dan meningkatkan sistem informasi perpustakaan agar tetap relevan dan efektif.
+			
+			<br>
+				2.	 Memastikan keamanan data pengguna dan menjaga privasi pengguna dalam penggunaan sistem informasi perpustakaan.
+			
+			<br>
+				3.	Mengelola koleksi perpustakaan digital dengan efisien dan memastikan keberlanjutan serta peningkatan koleksi sesuai dengan kebutuhan kurikulum.
+		</div>
+	</div>
+    <div class="col m6">
+        <div class="card-panel teal darken-2 white-text">
+            <h6><b>Identitas Perpustakaan</b></h6><hr>
+            <p>
+                Perpustakaan SMAN 1 BALAPULANG adalah tempat .
+            </p>
+        </div>
+    </div>
+</div>
